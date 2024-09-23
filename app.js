77580e01ec8be0b589d1b9609e83edd55def8df7
Alejandro García Navia <alejogarcia@unicauca.edu.co>
2024-09-23 21:43:20 +0000

    Test finalized

--- app.js
+++ app.js
@@ -13,13 +13,18 @@ let fromEuroToDollar =(euros) => {
     return euros *oneEuroIs.USD;
 };
 const fromDollarToYen=(dollars)=>{
-    let euros= dolars/oneEuroIs.USD;
+    let euros= dollars/oneEuroIs.USD;
     return euros*oneEuroIs.JPY
 };
 const fromYenToPound = (yens)=> {
     let euros= yens/oneEuroIs.JPY;
     return euros*oneEuroIs.GBP;
 };
+
+console.log(fromDollarToYen(10));
+console.log(fromEuroToDollar(10));
+console.log(fromYenToPound(1000));
+
 module.exports = {
     fromDollarToYen,
     fromEuroToDollar,
--- test.js
+++ test.js
@@ -9,36 +9,18 @@ test('adds 14 + 9 to equal 23', () => {
     // We expect the sum of those 2 numbers to be 23
     expect(total).toBe(23);
 });
-/*
-test("One euro should be 1.07 dollars", function() {
-    // Importo la funcion desde app.js
-    const { fromEuroToDollar } = require('./app.js');
 
-    // Uso la función como debe ser usada
-    const dollars = fromEuroToDollar(3.5);
+const { fromDollarToYen, fromEuroToDollar, fromYenToPound } = require('./app');
 
-    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
-    const expected = 3.5 * 1.07;
-
-    // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
-})
-test("One yen should be 0.87 pounds", function() {
-    // Importo la funcion desde app.js
-    const { fromYenToPound } = require('./app.js');
-
-    // Uso la función como debe ser usada
-    const pound = fromYenToPound(3.5);
-
-    // Si 1 yen son 0.87 pounds, entonces 3.5 yenes debe ser (3.5 * 0.87)
-    const expected = 3.5 * 0.87;
-
-    // Hago mi comparación (la prueba)
-    expect(fromYenToPound(3.5)).toBe(3.045); // 1 Yen son 0.87 pounds, entonces 3.5 yenes deberían ser = (3.5 * 0.87)
-})*/
-const {fromDollarToYen, fromYenToPound, fromEuroToDollar}=require('./app.js');
-test("10 puntos equivalen a 10.7 dolares",()=>{
-    let result=fromEuroToDollar(10)
-    let dolars=10*oneEuroIs.USD
-    expect(result).toBe(dolars)
+test('Convert 10 dollars to yen', () => {
+    expect(fromDollarToYen(10)).toBeCloseTo(1463.55, 2); 
 });
+
+test('Convert 10 euros to dollars', () => {
+    expect(fromEuroToDollar(10)).toBeCloseTo(10.7, 2); 
+});
+
+test('Convert 1000 yen to pounds', () => {
+    expect(fromYenToPound(1000)).toBeCloseTo(5.55, 2); 
+});
+
